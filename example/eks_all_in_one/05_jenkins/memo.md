6aade5bbbe2aac65e4e1b7b420c28a9e83376934
hacksungboo <qngkrtjd@naver.com>
2026-07-30 09:17:36 +0900

    jenkins 예제 추가함

---
+++ example/eks_all_in_one/05_jenkins/memo.md
@@ -0,0 +1,56 @@
+
+### jenkins 전용 efs storage class 를 만들어서 연결한다
+
+<img src="./assets/image01.png">
+
+
+### jenkins ingress rule 설정 
+
+<img src="./assets/image02.png">
+
+### jenkins 서버에 credentials 등록 
+<img src="./assets/image03.png">
+<img src="./assets/image04.png">
+<img src="./assets/image05.png">
+<img src="./assets/image06.png">
+<img src="./assets/image07.png">
+<img src="./assets/image08.png">
+<img src="./assets/image09.png">
+<img src="./assets/image10.png">
+
+### jenkins pipe line 만들기 위한 jenkins 플러그인 설치 
+
+<img src="./assets/image11.png">
+<img src="./assets/image12.png">
+<img src="./assets/image13.png">
+
+### pipe line 만들기 
+<img src="./assets/image14.png">
+<img src="./assets/image15.png">
+<img src="./assets/image16.png">
+<img src="./assets/image17.png">
+
+### github webhook 설정해서 테스트 하기
+
+<img src="./assets/image18.png">
+<img src="./assets/image19.png">
+<img src="./assets/image20.png">
+<img src="./assets/image21.png">
+<img src="./assets/image22.png">
+<img src="./assets/image23.png">
+
+
+### generic webhook 플러그인 설치후 [ci skip] 을 동작하게 만들기
+> pipe line 이 2번씩 실행되는것을 방지 하기 위해
+
+
+<img src="./assets/image24.png">
+<img src="./assets/image25.png">
+<img src="./assets/image26.png">
+<img src="./assets/image27.png">
+<img src="./assets/image28.png">
+<img src="./assets/image29.png">
+### github webhook 설정 수정 
+
+  <img src="./assets/image30.png">
+  <img src="./assets/image31.png">
---
+++ example/step10_jenkins_argocd/argocd/main.tf
@@ -0,0 +1,64 @@
+# argocd/main.tf
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30" 
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.14"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+ 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# helm provider 가 동작할 준비가 되어 있으면 "helm_release" 를 사용할수 있다.
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  # helm 저장소의 위치
+  repository       = "https://argoproj.github.io/argo-helm"
+  # chart 의 이름
+  chart            = "argo-cd"
+  # chart 버전
+  version          = "10.1.2"
+  # namespace 설정 
+  namespace        = "argocd"
+  create_namespace = true
+  # my-values.yaml 파일을 읽어서 설치 하도록 한다 
+  values = [ file("${path.module}/helm-values.yaml") ]
+  # 초기 비밀번호를 직접 설정하기 
+  set {
+    name  = "configs.secret.argocdServerAdminPassword"
+    # htpasswd (bcrypt) 형태로 변환하여 주입
+    value = bcrypt("admin1234") 
+  }
+}
+
+# helm provider 가 동작할 준비가 되어 있으면 "helm_release" 를 사용할수 있다.
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  # helm 저장소의 위치
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  # chart 의 이름
+  chart            = "ingress-nginx"
+  # chart 버전
+  version          = "4.11.0"
+  # namespace 설정 
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  set {
+    name = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
